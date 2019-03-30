897d24c843330c15d6db3e7beedbe7cf3129d3b0
Owen Sullivan <owen_sullivan@intuit.com>
2019-03-30 16:37:57 -0400

    Receiving and sending messages through web socket server

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
 import express from 'express';
+import * as http from 'http';
+import * as WebSocket from 'ws';
 
 const app = express();
-const port = 3000
+
+//initialize a simple http server
+const server = http.createServer(app);
+
+//initialize the WebSocket server instance
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+
+    //connection is up, let's add a simple simple event
+    ws.on('message', (message) => {
+
+        //log the received message and send it back to the client
+        console.log('received: %s', message);
+        ws.send(`Hello, you sent -> ${message}`);
+    });
+
+    //send immediatly a feedback to the incoming connection
+    ws.send('Hi there, I am a WebSocket server');
+});
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+//start our server
+server.listen(process.env.PORT || 5000, () => {
+    console.log(`Server started on port ${server.address().port} :)`);
+});
