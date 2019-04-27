7110e9333e23a6e61408bf38e411912c9865c440
Owen Sullivan <multiojuice@gmail.com>
2019-04-27 01:49:45 -0400

    Added all the handlers for the voice commands

--- src/handlers/change_screen.js
+++ src/handlers/change_screen.js
@@ -15,6 +15,15 @@ export const SCREEN_IDS = {
   COMPONENTS: handle_basic
 }
 
+export const change_from_dialog = (req, res, id) => {
+  if (SCREEN_IDS[id]) {
+    SCREEN_IDS[id](id);
+    res.send('SUCCESS')
+  } else {
+    res.send('ERROR: NO SUCH SCREEN ID')
+  }
+}
+
 const change = (req, res) => {
   if (SCREEN_IDS[req.params.screenId]) {
     SCREEN_IDS[req.params.screenId](req.params.screenId);
--- src/handlers/voice.js
+++ src/handlers/voice.js
@@ -1,12 +1,71 @@
 import {wss} from '../index';
+import { back } from '.';
+import { change_from_dialog } from './change_screen';
+
+const handlePlay = (arr) => {
+    fetch('http://musicsystem-imagine-rit-music-player.cs.house/resume');
+}
+
+const handleSpotify = (arr) => {
+    switch (arr[1]) {
+        case 'PAUSE':
+            fetch('http://musicsystem-imagine-rit-music-player.cs.house/pause');
+            break;
+        case 'VOLUME':
+            fetch(`http://musicsystem-imagine-rit-music-player.cs.house/volume?volume=${toLowerCase(arr[2])}`);
+            break;
+        case 'SKIP_BACKWARD':
+            fetch('http://musicsystem-imagine-rit-music-player.cs.house/change?forward=false');
+            break;
+        case 'SKIP_FORWARD':
+            fetch('http://musicsystem-imagine-rit-music-player.cs.house/change?forward=true')
+            break;
+        case 'PLAY':
+            handlePlay(arr);
+            break;
+    }
+}
+
+
+const handleDisplay = (req, res, arr) => {
+    switch (arr[1]) {
+        case 'ON':
+
+            break;
+        case 'OFF':
+            break;
+
+        case 'BACK':
+            back(req, res);
+            break;
+
+        case 'SHOW':
+            change_from_dialog(req, res, arr[2]);
+            break;
+    }
+}
+
 
 const voice = (req, res) => {
     console.log(req.body);
+    const arr = req.body.queryResult.fulfillmentText.split(':');
+
+    switch (arr[0]) {
+        case 'SPOTIFY':
+            handleSpotify(arr);
+            break;
+        case 'DISPLAY':
+            handleDisplay(req, res, arr);
+            break;
+        default:
+            break;
+    }
+
     wss.clients.forEach(client => {
         console.warn('sending voice');
-        client.send(`VOICE|${JSON.stringify(req.body)}`);
+        client.send(`VOICE|${JSON.stringify(req.body.queryResult.fulfillmentText)}`);
       });
-    res.send(req.body);
+    res.send(200);
 }
 
 export default voice;
